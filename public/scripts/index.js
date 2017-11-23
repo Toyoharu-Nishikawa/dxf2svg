2f1d49ef93d4f7ca266c8a8b5eb131e1cd52d6fb
toyoharu nishikawa <toyohal@email.com>
2017-11-23 22:05:06 +0900

    delete unnecessary codes

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -5,27 +5,6 @@ $(document).ready(function() {
     var fileExtension = null;
     var filename = null;
     var svgString = null;
-    var kicadPcb = null;
-    var kicadPcbToBeAppended = null;
-
-    var requiredFeatures = ['svg', 'inlinesvg', 'blobconstructor', 'filereader'];
-    var missingFeatures = [];
-    requiredFeatures.forEach(function(feature) {
-        if (!Modernizr[feature]) {
-            missingFeatures.append(feature);
-        }
-    });
-    missingFeatures = missingFeatures.join(', ');
-
-    if (missingFeatures) {
-        $('.upgrade-browser-notification').show();
-        $('#missing-features').html(missingFeatures);
-    }
-
-    var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
-    if (isFirefox) {
-        $('.firefox-notification').show();
-    }
 
     $('#input-file-chooser').change(function() {
         var file = this.files[0];
@@ -41,17 +20,11 @@ $(document).ready(function() {
             switch (fileExtension) {
                 case 'dxf':
                     svgString = dxfToSvg(fileData);
-                    $('#svg-and-kicad-pcb-save-links, #segmentation-notification, #dxf-segmentation-notification').show();
-                    $('#svg-segmentation-notification, .invalid-input').hide();
                     break;
                 case 'svg':
                     svgString = fileData;
-                    $('#kicad-pcb-save-link, #svg-segmentation-notification, #segmentation-notification').show();
-                    $('#dxf-segmentation-notification, .invalid-input').hide();
                     break;
                 default:
-                    $('#no-save-link, .invalid-input').show();
-                    $('#segmentation-notification').hide();
                     return;
             }
 
@@ -64,84 +37,17 @@ $(document).ready(function() {
             var dataUri = 'data:image/svg+xml;utf8,' + encodeURIComponent(svgString);
             var svgImage = $('<img>', {id:'svg-image', src:dataUri});
             $('#svg-image-container').append(svgImage);
-            kicadPcb = svgToKicadPcbGetter(svgString);
         })[0].readAsText(file);
     });
 
-    $('#appended-file-chooser').change(function() {
-        var file = this.files[0];
-        $(new FileReader()).load(function(event) {
-            var fileData = event.target.result;
-            fileData = _.trim(fileData);
-
-            var isFileValid = _(fileData).startsWith('(kicad_pcb ') && _(fileData).endsWith(')');
-            if (isFileValid) {
-                kicadPcbToBeAppended = fileData.substr(0, fileData.length-1);
-                $('#invalid-append-file').hide();
-            } else {
-                kicadPcbToBeAppended = null;
-                $('#invalid-append-file').show();
-            }
-        })[0].readAsText(file);
-    });
-
-    $('.save-svg-link').click(function() {
+      $('#save-svg-link_new').click(function() {
         if (fileExtension === 'svg' || !svgString) {
             return;
         }
         saveStringAsFile(svgString, bareFilename+'.svg');
     });
 
-    $('.save-kicad-pcb-link').click(function() {
-        if (!kicadPcb) {
-            return;
-        }
-        kicadPcb = svgToKicadPcbGetter(svgString);
-        saveStringAsFile(kicadPcb, bareFilename+'.kicad_pcb');
-    });
-
-    $('#advanced-options-link').click(function() {
-        $("#advanced-options").show();
-        $("#advanced-options-expander").hide();
-    });
-
-    $('#translation-x, #translation-y').change(function() {
-        var id = this.id;
-        var value = parseFloat($('#'+id).val());
-        if (isNaN(value)) {
-            $('#invalid-input-'+id).show();
-        } else {
-            $('#invalid-input-'+id).hide();
-        }
-    });
-
-    $('#report-link').click(function() {
-        $("#report-help").show();
-    });
-
-    $('#donate-link').click(function() {
-        $("#paypal-submit").trigger("click");
-        return false;
-    });
-
-    function svgToKicadPcbGetter(svgString)
-    {
-        var translationX = parseFloat($('#translation-x').val());
-        if (isNaN(translationX)) {
-            translationX = 0;
-        }
-
-        var translationY = parseFloat($('#translation-y').val());
-        if (isNaN(translationY)) {
-            translationY = 0;
-        }
-
-        var layer = $('#layer').val();
-
-        return svgToKicadPcb(svgString, filename, layer, translationX, translationY, kicadPcbToBeAppended, fileExtension === 'dxf');
-    }
-
-    function saveStringAsFile(string, filename)
+      function saveStringAsFile(string, filename)
     {
         var blob = new Blob([string], {type: 'text/plain; charset=utf-8'});
         saveAs(blob, filename);
