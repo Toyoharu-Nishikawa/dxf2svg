04ccffd261552bba1e30db8c66137cbba0a7dcf0
toyoharu nishikawa <toyohal@email.com>
2017-12-22 01:30:05 +0900

    add viewer with svg.js

--- public/dxf2svg/scripts/index.js
+++ public/dxf2svg/scripts/index.js
@@ -34,9 +34,34 @@ $(document).ready(function() {
                 return;
             }
 
-            var dataUri = 'data:image/svg+xml;utf8,' + encodeURIComponent(svgString);
-            var svgImage = $('<img>', {id:'svg-image', src:dataUri});
-            $('#svg-image-container').append(svgImage);
+            let default_width =600;
+            let default_height =600;
+            let draw = SVG("svg-image-container").panZoom({zoomFactor:1.1});
+            draw.width(default_width);
+            draw.height(default_height);
+            draw.attr('preserveAspectRatio', 'xMinYMin slice');
+            draw.style( {
+              border: '1px solid #F5F5F5',
+              margin:0,
+              padding:0,
+              background:'linear-gradient(to bottom, white, RoyalBlue)'
+            });
+            draw.viewbox(0, 0, default_width, default_height).flip("y");
+            let background = draw.group();
+             background.line(0, 0, 1000, 0).fill("none").stroke({color:"black",opacity: 1.0,width:1})
+              .attr("vector-effect", "non-scaling-stroke")
+              .attr("stroke-dasharray","5 5");
+            background.line(0, 0, 0, 1000).fill("none").stroke({color:"black",opacity: 1.0,width:1})
+             .attr("vector-effect", "non-scaling-stroke")
+             .attr("stroke-dasharray","5 5");
+            draw.screen = draw.group();
+            draw.screen.background = background;
+            draw.screen.sheet = draw.screen.svg(svgString)
+              .stroke({color:'blue',opacity: 1.0,width:1})
+              .fill('none')
+              .attr("stroke-linecap", "round")
+              .attr("stroke-linejoin", "round");
+            console.log(draw.screen.sheet.svg()) 
         })[0].readAsText(file);
     });
 
